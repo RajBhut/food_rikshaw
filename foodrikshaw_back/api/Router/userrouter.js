68049e898ce2cad1052b58b164dd208ce689c811
RajBhut214 <rajbhut2832005@gmail.com>
2024-10-12 00:35:20 +0530

    chenge

--- foodrikshaw_back/api/Router/userrouter.js
+++ foodrikshaw_back/api/Router/userrouter.js
@@ -44,6 +44,7 @@ Userrouter.get('/', (req, res) => {
 });
 
 Userrouter.post('/', async (req, res) => {
+    console.log(req.body);
     const { name, email, password } = req.body;
     try {
         const user = await User.create({ name, email, password });
