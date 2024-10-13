1e93d75e790e5654c90e60bfb8c83e3561273db1
RajBhut214 <rajbhut2832005@gmail.com>
2024-10-13 19:28:37 +0530

    cors

--- foodrikshaw_back/api/Router/productroter.js
+++ foodrikshaw_back/api/Router/productroter.js
@@ -8,37 +8,20 @@ export const productrouter = Router();
 function generateETag(data) {
     return crypto.createHash('md5').update(JSON.stringify(data)).digest('hex');
 }
-// productrouter.get('/', dbConnectionMiddleware, async (req, res) => {
-//     try {
-//         const products = await Product.find().select([
-//             '-__v',
-//             '-createdAt',
-//             '-updatedAt',
-//         ]);
-//         res.header('Access-Control-Allow-Origin', 'https://food.rajb.codes');
-//         res.header('Access-Control-Allow-Credentials', 'true');
-//         res.json(products);
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
+
 productrouter.get('/', dbConnectionMiddleware, async (req, res) => {
     try {
-        // Fetch products
         const products = await Product.find().select(['-__v', '-createdAt']);
 
-        // Generate ETag and Last-Modified
-        const eTag = generateETag(products); // ETag is a hash of the product data
+        const eTag = generateETag(products);
         const lastModified = products.length
             ? products[0].updatedAt
             : new Date();
 
-        // Set CORS headers
         res.header('Access-Control-Allow-Origin', 'https://food.rajb.codes');
         res.header('Access-Control-Allow-Credentials', 'true');
         res.header('Access-Control-Expose-Headers', 'ETag, Last-Modified');
-
+        console.log(res.header);
         if (req.headers['if-none-match'] === eTag) {
             return res.status(304).send();
         }
@@ -47,7 +30,7 @@ productrouter.get('/', dbConnectionMiddleware, async (req, res) => {
             req.headers['if-modified-since'] &&
             new Date(req.headers['if-modified-since']) >= new Date(lastModified)
         ) {
-            return res.status(304).send(); // Not Modified, use cached version
+            return res.status(304).send();
         }
         res.setHeader('ETag', eTag);
 
