5d07e2712b8d3d2be0623e1ed59c389e5831004a
RajBhut214 <rajbhut2832005@gmail.com>
2024-10-15 19:34:16 +0530

    add

--- foodrikshaw_back/api/cache.js
+++ foodrikshaw_back/api/cache.js
@@ -1,5 +1,5 @@
 import NodeCache from 'node-cache';
 
 export const orderCache = new NodeCache({ stdTTL: 300 });
-export const purchaseCache = new NodeCache({ stdTTL: 300 });
+export const purchaseCache = new NodeCache({ stdTTL: 9000 });
 export const productCache = new NodeCache({ stdTTL: 300 });
